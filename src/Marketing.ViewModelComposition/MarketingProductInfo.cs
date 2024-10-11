85747c5aba557fef7113ed944ffc9af4bf64403c
SissgerZ <fuessinger94@web.de>
2024-10-11 21:39:25 +0200

    Initial commit

---
+++ src/Marketing.ViewModelComposition/MarketingProductInfo.cs
@@ -0,0 +1,16 @@
+﻿namespace Marketing.ViewModelComposition;
+
+public class MarketingProductInfo : ICompositionRequestsHandler
+{
+    [HttpGet("/product/{id}")]
+    public Task Handle(HttpRequest request)
+    {
+        var vm = request.GetComposedResponseModel();
+
+        //retrieve product details from the marketing database or service
+        vm.ProductName = "Sample product";
+        vm.ProductDescription = "This is a sample product";
+
+        return Task.CompletedTask;
+    }
+}
---
+++ src/Sales.ViewModelComposition/Usings.cs
@@ -0,0 +1,4 @@
+﻿global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Routing;
+global using ServiceComposer.AspNetCore;
